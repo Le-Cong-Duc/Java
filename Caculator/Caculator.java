5a30af597d3f70f597811a712b1ec8676bdc7335
Le Cong Duc <congduc1801@gmail.com>
2023-11-12 15:02:14 +0700

    Caculator

--- Caculator/Caculator.java
+++ Caculator/Caculator.java
@@ -3,6 +3,7 @@ package Caculator;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -38,6 +39,7 @@ public class Caculator extends JFrame {
 		setBounds(100, 100, 500, 400);
 		setTitle("Caculator");
 		setLocationRelativeTo(null);
+		
 		JPanel background = new JPanel();
 		setContentPane(background);
 		background.setLayout(null);
@@ -45,41 +47,67 @@ public class Caculator extends JFrame {
 		JPanel view1 = new JPanel();
 		view1.setBounds(34, 11, 419, 60);
 		background.add(view1);
-		view1.setLayout(null);
+		view1.setLayout(new BorderLayout());
 
 		JTextField text = new JTextField();
-		text.setBounds(0, 11, 409, 50);
+		text.setBounds(0, 0, 409, 50);
 		text.setFont(new Font("Times New Roman", Font.PLAIN, 40));
 		view1.add(text);
 
 		JPanel view2 = new JPanel();
 		view2.setBounds(34, 100, 407, 233);
-		view2.setLayout(new FlowLayout(FlowLayout.LEFT, 4, 4));
+		view2.setLayout(new GridLayout(4,5));
 		background.add(view2);
 		
-		JButton button1 = new JButton("1");
-		button1.setBounds(200, 100, 100, 100);
-		view2.add(button1);
+		JButton button7 = new JButton("7");
+		view2.add(button7);
 		
-		JButton button2 = new JButton("2");
-		button2.setBounds(200, 100, 100, 100);
-		view2.add(button2);
+		JButton button8 = new JButton("8");	
+		view2.add(button8);
 		
-		JButton button3 = new JButton("3");
-		button3.setBounds(200, 100, 100, 100);
-		view2.add(button3);
+		JButton button9 = new JButton("9");
+		view2.add(button9);
+		
+		JButton buttonCong = new JButton("+");
+		view2.add(buttonCong);
 		
 		JButton button4 = new JButton("4");
-		button4.setBounds(200, 100, 100, 100);
 		view2.add(button4);
 		
 		JButton button5 = new JButton("5");
-		button5.setBounds(200, 100, 100, 100);
 		view2.add(button5);
 		
 		JButton button6 = new JButton("6");
-		button6.setBounds(200, 100, 100, 100);
 		view2.add(button6);
+		
+		JButton buttonTru = new JButton("-");
+		view2.add(buttonTru);
+		
+		JButton button1 = new JButton("1");
+		view2.add(button1);
+		
+		JButton button2 = new JButton("2");	
+		view2.add(button2);
+		
+		JButton button3 = new JButton("3");
+		view2.add(button3);
+		
+		JButton buttonNhan = new JButton("x");
+		view2.add(buttonNhan);
+		
+		JButton button0 = new JButton("0");
+		view2.add(button0);
+		
+		JButton buttonCham = new JButton(".");
+		view2.add(buttonCham);
+		
+		JButton result = new JButton("=");
+		view2.add(result);
+		
+		JButton buttonChia = new JButton("/");
+		view2.add(buttonChia);
+		
+		
 
 	}
 }
