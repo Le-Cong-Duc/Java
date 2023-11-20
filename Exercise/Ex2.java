4a9b0710951d00f676e91bc9818e8c4eb8635b55
Le Cong Duc <congduc1801@gmail.com>
2023-11-20 21:12:15 +0700

    Test

--- Exercise/Ex2.java
+++ Exercise/Ex2.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Color;
 
 public class Ex2 extends JFrame {
 
@@ -48,17 +49,19 @@ public class Ex2 extends JFrame {
 		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
+		panel.setForeground(new Color(128, 255, 0));
 		panel.setLocation(30, 11);
 		panel.setSize(500, 300);
 		contentPane.add(panel);
 		panel.setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("0");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(214, 71, 31, 14);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 37));
+		lblNewLabel.setBounds(234, 57, 94, 74);
 		panel.add(lblNewLabel);
 
-		JButton btnNewButton = new JButton("Click");
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setForeground(new Color(0, 255, 0));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				cout++;
@@ -66,8 +69,22 @@ public class Ex2 extends JFrame {
 			}
 
 		});
-		btnNewButton.setBounds(144, 151, 169, 74);
+		btnNewButton.setBounds(100, 151, 140, 50);
 		panel.add(btnNewButton);
+
+		JButton btnNewButton1 = new JButton("Sub");
+		btnNewButton1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cout--;
+				lblNewLabel.setText(" " + cout);
+
+			}
+		});
+
+		btnNewButton1.setBounds(300, 151, 140, 50);
+		panel.add(btnNewButton1);
 
 	}
 }
